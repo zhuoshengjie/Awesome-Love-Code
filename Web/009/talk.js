cbef3cbcedbbeade80cd1d002e5cc332893d87a7
zhuoshengjie <47313110+zhuoshengjie@users.noreply.github.com>
2024-10-22 14:06:17 +0800

    Update talk.js

--- Web/009/talk.js
+++ Web/009/talk.js
@@ -1,6 +1,6 @@
 $(function() {
     $('#yes').click(function(event) {
-        modal('我就知道小宝宝一定会愿意~(^_^)', function() {
+        modal('我就知道饭饭一定会愿意~(^_^)', function() {
             $('.page_one').addClass('hide');
             $('.page_two').removeClass('hide');
             // typeWrite();
@@ -9,59 +9,59 @@ $(function() {
         });
     });
     $('#no').click(function(event) {
-        modal('明人不说暗话！', A);
+        modal('天气凉了要记得加衣服', A);
     });
 });
 
 function A() {
-    modal('我爱你！', B);
+    modal('买点吃的放宿舍晚上会饿喔', B);
 }
 
 function B() {
-    modal('我知道你在等我这一句话', C);
+    modal('出去玩的时候一定要少喝酒', C);
 }
 
 function C() {
-    modal('请宝宝不要拒绝我嘛~', D);
+    modal('打游戏不要玩太晚啦', D);
 }
 
 function D() {
-    modal('拒绝我，不存在的', E);
+    modal('好好睡觉', E);
 }
 
 function E() {
-    modal('这辈子都不可能让你离开我的!!!', F);
+    modal('1000字检讨', F);
 }
 
 function F() {
-    modal('跟我走吧宝宝~', G);
+    modal('我错啦', G);
 }
 
 function G() {
-    modal('房产证上写你的名字', H);
+    modal('请饭饭一定要原谅我嘛', H);
 }
 
 function H() {
-    modal('我会做饭', I);
+    modal('走着走着走到Coco了', I);
 }
  function I() {
-    modal('我会买很多东西给你', J)
+    modal('想喝奶茶扣1', J)
 }
-function I() {
-    modal('会好好疼你', J)
-}
-function I() {
-    modal('会哄你睡觉', J)
-}
-function I() {
-    modal('会唱歌给你听', J)
-}
-function I() {
-    modal('爱你。么么哒！', J)
-}
+// function I() {
+//     modal('会好好疼你', J)
+// }
+// function I() {
+//     modal('会哄你睡觉', J)
+// }
+// function I() {
+//     modal('会唱歌给你听', J)
+// }
+// function I() {
+//     modal('爱你。么么哒！', J)
+// }
 
 function J() {
-    modal('行，我们去民政局登记吧', function() {
+    modal('喝了我的奶茶不许拒绝我啦', function() {
         fireworks();
     });
 }
